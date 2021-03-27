e55ea6797a80ed6dd4b542ab71a135efe65b8549
Brekun Aleksandra <alexa97alexa19@gmail.com>
2021-03-27 11:30:18 +0300

    Добавлен новый экран, установлена иконка приложения в виде якоря

--- app/src/main/java/com/example/androidbrekun/FirstActivity.java
+++ app/src/main/java/com/example/androidbrekun/FirstActivity.java
@@ -2,7 +2,14 @@ package com.example.androidbrekun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.androidbrekun.ui.login.LoginActivity;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -10,5 +17,17 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.test_textView);
+        final EditText editText = findViewById(R.id.test_edit_text);
+
+        Button button = findViewById(R.id.test_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
